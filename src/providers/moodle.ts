5ea7a273b1f0988a73e3d567054e1612d0793fd2
Oliver Enes <oliver@enesnet.de>
2019-10-19 00:17:42 +0200

    moodle Provider, getPlan

--- src/providers/moodle.ts
+++ src/providers/moodle.ts
@@ -8,16 +8,20 @@ export class MoodleProvider {
    * @param password
    */
   public static async login(encryptedCredentials: string): Promise<string> {
-      const response: any = await ((await fetch(MoodleProvider.middlewareUrl + "/secureMoodleSession?secret=" + encodeURIComponent(encryptedCredentials))).json());
-      if (response.error) {
-        throw new Error(response.error);
-      } else {
-        const moodleSession = response.moodleSession;
-        return (moodleSession);
-      }
+    const response: any = await ((await fetch(MoodleProvider.middlewareUrl + "/secureMoodleSession?secret=" + encodeURIComponent(encryptedCredentials))).json());
+    if (response.error) {
+      throw new Error(response.error);
+    } else {
+      const moodleSession = response.moodleSession;
+      return (moodleSession);
+    }
   }
 
   public static async getPublicKey(): Promise<any> {
     return (((await fetch(this.middlewareUrl + "/publickey")).text()));
+  }
+
+  public static async getPlan(day: 'today' | 'tomorrow', moodleSession: string): Promise<any> {
+    return await (await fetch(MoodleProvider.middlewareUrl + "/fetch/" + day + "?moodleSession=" + moodleSession)).json();
   }
 }
