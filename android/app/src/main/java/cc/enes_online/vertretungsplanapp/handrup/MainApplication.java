07c3328e78a5818134aa1438eb1019b77259de36
Oliver Enes <oliver@enesnet.de>
2020-09-16 19:12:42 +0200

    native modules

--- android/app/src/main/java/cc/enes_online/vertretungsplanapp/handrup/MainApplication.java
+++ android/app/src/main/java/cc/enes_online/vertretungsplanapp/handrup/MainApplication.java
@@ -3,6 +3,8 @@ package cc.enes_online.vertretungsplanapp.handrup;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.picker.RNCPickerPackage;
+import io.expo.appearance.RNCAppearancePackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCPickerPackage(),
+            new RNCAppearancePackage(),
             new SplashScreenReactPackage(),
             new SvgPackage(),
             new RNGestureHandlerPackage(),
