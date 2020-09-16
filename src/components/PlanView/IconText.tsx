1507da81f375e735a4731a6b74153b632032a02f
Oliver Enes <oliver@enesnet.de>
2020-09-16 19:37:38 +0200

    dark mode

--- src/components/PlanView/IconText.tsx
+++ src/components/PlanView/IconText.tsx
@@ -2,6 +2,7 @@ import React, {Component, ReactNode} from "react";
 import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
 import {Text, View, StyleSheet} from "react-native";
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {ThemeContext} from "../themeContext/theme-context";
 
 interface IconTextProps {
   text: string;
@@ -10,25 +11,38 @@ interface IconTextProps {
 }
 
 export class IconText extends Component<IconTextProps> {
+
+  static contextType = ThemeContext;
+
+  private getStyles() {
+    const darkMode = this.context.theme === 'dark';
+    return styles(darkMode);
+  }
+
+  private getIconColor() {
+    const darkMode = this.context.theme === 'dark';
+    return darkMode ? 'white' : 'black';
+  }
+
   public render(): ReactNode {
     if (!this.props.text) return null;
 
     if (this.props.text2) {
       return (
           <>
-            <View style={{flexDirection: 'row', margin: 10}}>
-              <Text style={styles.text}>{this.props.text}</Text>
-              <FontAwesomeIcon icon={this.props.icon} size={30}/>
-              <Text style={styles.text}>{this.props.text2}</Text>
+            <View style={{flexDirection: 'row', margin: 20, alignItems: 'center', justifyContent: 'flex-start'}}>
+              <Text style={this.getStyles().text}>{this.props.text}</Text>
+              <FontAwesomeIcon color={this.getIconColor()} icon={this.props.icon} size={30}/>
+              <Text style={this.getStyles().text}>{this.props.text2}</Text>
             </View>
           </>
       );
     } else {
       return (
           <>
-            <View style={{flexDirection: 'row', margin: 10}}>
-              <FontAwesomeIcon icon={this.props.icon} size={30}/>
-              <Text style={styles.text}>{this.props.text}</Text>
+            <View style={{flexDirection: 'row', margin: 20, alignItems: 'center', justifyContent: 'flex-start'}}>
+              <FontAwesomeIcon color={this.getIconColor()} icon={this.props.icon} size={30}/>
+              <Text style={this.getStyles().text}>{this.props.text}</Text>
             </View>
           </>
       );
@@ -36,12 +50,12 @@ export class IconText extends Component<IconTextProps> {
   }
 }
 
-const styles = StyleSheet.create({
+const styles = (darkMode: boolean) => StyleSheet.create({
   text: {
     padding: 5,
     margin: 10,
     fontSize: 18,
-    lineHeight: 18,
-    color: 'black',
+    lineHeight: 20,
+    color: darkMode ? 'white' : 'black',
   }
 });
