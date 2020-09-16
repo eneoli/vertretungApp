afdc3ec53c303a70a1cbb01de8be8e6b1f6fd1c4
Oliver Enes <oliver@enesnet.de>
2020-09-16 17:50:29 +0200

    dark mode

--- src/components/InfoHeader/index.tsx
+++ src/components/InfoHeader/index.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import {StyleSheet, View, Text} from "react-native";
 import moment from 'moment';
 import 'moment/locale/de'
+import {ThemeContext} from "../themeContext/theme-context";
 
 interface IInfoHeaderProps {
   missingTeachers: any;
@@ -11,34 +12,50 @@ interface IInfoHeaderProps {
 }
 
 export class InfoHeader extends Component<IInfoHeaderProps> {
+
+  static contextType = ThemeContext;
+
   constructor(props: IInfoHeaderProps) {
     super(props);
     moment.locale('de');
   }
+
+  private getStyles() {
+    return getStyles(this.context.theme === 'dark');
+  }
+
   public render(): ReactNode {
     return (
-        <View style={styles.root}>
-          <Text style={styles.dayText}>{moment(this.props.day).format('dddd, DD.MM')}</Text>
-          <Text style={styles.boldText}>Fehlende Lehrer:</Text>
-          <Text>{this.props.missingTeachers}</Text>
-          <Text style={styles.boldText}>Verplante Lehrer:</Text>
-          <Text>{this.props.usedTeachers}</Text>
+        <View style={this.getStyles().root}>
+          <Text
+              style={this.getStyles().dayText}>{moment(this.props.day).format('dddd, DD.MM')}</Text>
+          <Text style={this.getStyles().boldText}>Fehlende Lehrer:</Text>
+          <Text style={this.getStyles().normalText}>{this.props.missingTeachers}</Text>
+          <Text style={this.getStyles().boldText}>Verplante Lehrer:</Text>
+          <Text style={this.getStyles().normalText}>{this.props.usedTeachers}</Text>
         </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  root: {
-    margin: 15,
-    padding: 15,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  dayText: {
-    marginBottom: 10,
-    fontSize: 20,
-    fontWeight: 'bold',
-  }
-});
+const getStyles = (darkMode: boolean) => {
+  return StyleSheet.create({
+    root: {
+      margin: 15,
+      padding: 15,
+    },
+    boldText: {
+      fontWeight: 'bold',
+      color: darkMode ? 'white' : 'black'
+    },
+    dayText: {
+      marginBottom: 10,
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: darkMode ? 'white' : 'black'
+    },
+    normalText: {
+      color: darkMode ? 'white' : 'black',
+    }
+  });
+};
