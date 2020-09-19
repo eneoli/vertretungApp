03c9e044555081cf806111a8e6a39e827f5ce158
Oliver Enes <oliver@enesnet.de>
2020-09-19 14:07:59 +0200

    info component

---
+++ src/components/Info/info.tsx
@@ -0,0 +1,54 @@
+import React, {Component, ReactNode} from "react";
+import {Image, ImageBackground, Modal, Text, StyleSheet, View, TouchableOpacity} from "react-native";
+import DeviceInfo from 'react-native-device-info'
+
+interface InfoProps {
+  onClose: () => void;
+}
+
+export class Info extends Component<InfoProps> {
+  public render(): ReactNode {
+    return (
+        <Modal visible={true} transparent={true} animationType={"slide"} animated={true} onRequestClose={this.props.onClose}>
+          <ImageBackground style={{flex: 1}} source={require('./../../../assets/background.png')}>
+            <View style={styles.container}>
+              <Text style={styles.header}>Vertretungsplan App Gymnasium Leoninum Handrup</Text>
+              <TouchableOpacity>
+                <Image source={require('../../../assets/icon.png')} style={styles.logo}/>
+              </TouchableOpacity>
+              <Text style={styles.text}>App-Version: {DeviceInfo.getVersion()}</Text>
+              <Text style={styles.text}>Entwickelt von Oliver Enes (oliverenes01@gmail.com) im Auftrag des
+                Schülerrates</Text>
+            </View>
+          </ImageBackground>
+        </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    padding: 25,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  header: {
+    color: 'white',
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  text: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  }
+});
