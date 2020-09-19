22f82279ec35df066e99a27c683ae65a97738782
Oliver Enes <oliver@enesnet.de>
2020-09-19 12:51:33 +0200

    preparations for notch support

--- android/app/src/main/java/cc/enes_online/vertretungsplanapp/handrup/MainActivity.java
+++ android/app/src/main/java/cc/enes_online/vertretungsplanapp/handrup/MainActivity.java
@@ -6,6 +6,8 @@ import com.facebook.react.ReactRootView;
 import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.view.WindowManager;
+import android.os.Build;
 
 public class MainActivity extends ReactActivity {
 
@@ -31,6 +33,16 @@ public class MainActivity extends ReactActivity {
       @Override
       protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this, true);
+
+/*
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+            layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(layoutParams);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+*/
         super.onCreate(savedInstanceState);
     }
 }
