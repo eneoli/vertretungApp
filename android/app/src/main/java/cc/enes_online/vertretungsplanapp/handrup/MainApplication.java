c77612b258e8dd8d812ca47e0ff6f7ae73ec8d3c
Oliver Enes <oliver@enesnet.de>
2020-09-19 14:26:28 +0200

    linking, new icon

--- android/app/src/main/java/cc/enes_online/vertretungsplanapp/handrup/MainApplication.java
+++ android/app/src/main/java/cc/enes_online/vertretungsplanapp/handrup/MainApplication.java
@@ -3,6 +3,7 @@ package cc.enes_online.vertretungsplanapp.handrup;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactnativecommunity.picker.RNCPickerPackage;
 import io.expo.appearance.RNCAppearancePackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNCPickerPackage(),
             new RNCAppearancePackage(),
             new SplashScreenReactPackage(),
