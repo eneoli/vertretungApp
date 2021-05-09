185acaad0aa0e1d5c90cb1ac66e21ba94c213fdb
Oliver Enes <oliver@enesnet.de>
2021-05-09 23:12:34 +0200

    use new settings manager

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,5 +1,5 @@
 import {Component} from "react";
-import {View, Text, AsyncStorage, StyleSheet, TouchableOpacity} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 import * as React from 'react';
 import {observable} from "mobx";
 import {observer} from "mobx-react";
@@ -8,6 +8,7 @@ import {Picker} from '@react-native-community/picker';
 import {ThemeContext} from "../themeContext/theme-context";
 import {Appearance} from "react-native-appearance";
 import {CoursePicker} from "./course-picker";
+import {AppContext, AppTheme, SettingsManager} from "../../providers/settings";
 
 interface ISettingsProps {
   navigation: NavigationScreenProp<this>;
@@ -19,56 +20,37 @@ export class Settings extends Component<ISettingsProps> {
   static contextType = ThemeContext;
 
   @observable
-  private selectedYear: number = -1;
-  @observable
-  private selectedClass: string = null;
-  private selectedCourses: string[] = [''];
-  @observable
-  private theme: string;
+  private isInitialized: boolean = false;
+
+  @observable.deep
+  private settings: AppContext;
+
+  private settingsManager: SettingsManager;
+
   private classes = [5, 6, 7, 8, 9, 10, 11, 12, 13];
+
   private classLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 
   constructor(props) {
     super(props);
-    AsyncStorage.getItem('class').then((studentClass) => {
-      this.selectedYear = parseInt(studentClass.replace(/[a-z]/g, ""), 10);
-      this.selectedClass = studentClass.replace(/[0-9]/g, "");
-    });
-
-    AsyncStorage.getItem('courses').then((courses) => {
-      if (courses) {
-        this.selectedCourses = JSON.parse(courses);
-      }
-    });
-
-    AsyncStorage.getItem('theme').then((theme) => {
-      this.theme = theme.toString() || 'system';
+    this.settingsManager = new SettingsManager();
+    this.settingsManager.load().then((settings) => {
+      this.settings = settings;
+      this.isInitialized = true;
     });
   }
 
   componentWillUnmount(): void {
-    if (this.selectedYear == -1) {
-      AsyncStorage.setItem('class', '');
-    } else {
-      if (!this.selectedClass || this.selectedYear > 10) {
-        AsyncStorage.setItem('class', '' + this.selectedYear);
-      } else {
-        AsyncStorage.setItem('class', '' + this.selectedYear + this.selectedClass);
-      }
+    this.settingsManager.setSettings(this.settings);
+    this.settingsManager.save();
 
-      if (this.selectedYear > 10) {
-        AsyncStorage.setItem('courses', JSON.stringify(this.selectedCourses));
-      }
-    }
-
-    AsyncStorage.setItem('theme', this.theme);
     const colorScheme = Appearance.getColorScheme();
     const systemLightMode = colorScheme === 'light' || colorScheme === 'no-preference';
 
-    if (this.theme === 'system') {
+    if (this.settings.theme === 'system') {
       this.context.setTheme(systemLightMode ? 'light' : 'dark');
     } else {
-      this.context.setTheme(this.theme);
+      this.context.setTheme(this.settings.theme);
     }
   }
 
@@ -100,68 +82,85 @@ export class Settings extends Component<ISettingsProps> {
     });
   }
 
+  private onGradeChange(grade: number) {
+    if (grade === null) {
+      this.settings.classSettings.grade = null;
+    } else {
+      this.settings.classSettings.grade = grade;
+    }
+  }
+
+  private onClassChange(className: string) {
+    this.settings.classSettings.className = className;
+  }
+
+  private onCourseChange(courses: string[]) {
+    this.settings.classSettings.courses = [...courses];
+  }
+
+  private onThemeChange(theme: string) {
+    this.settings.theme = theme as AppTheme;
+  }
+
   public render(): React.ReactNode {
+
+    if (!this.isInitialized) {
+      return null;
+    }
+
     return (
         <View style={this.getStyles().view}>
           <Text style={this.getStyles().headerText}>Hier kannst du deine Klasse einstellen, damit
             eventuelle Vertretungsstunden besonders hervorgehoben werden
             und du sie so besser im Blick hast.</Text>
           <Text style={this.getStyles().fieldText}>Jahrgang</Text>
-          <Picker selectedValue={this.selectedYear} mode={'dialog'} prompt={'Jahrgang'}
+          <Picker selectedValue={this.settings.classSettings.grade}
+                  mode={'dialog'}
+                  prompt={'Jahrgang'}
                   style={this.getStyles().picker}
                   itemStyle={this.getStyles().pickerItems}
-                  onValueChange={(v) => {
-                    this.selectedYear = parseInt(v.toString(), 10);
-                    if (this.selectedYear > 10 || this.selectedYear == -1) {
-                      this.selectedClass = null;
-                    }
-                  }}>
-            <Picker.Item label={'Nicht angeben'} value={-1} color={this.getStyles().pickerItems.color}/>
+                  onValueChange={(value) => this.onGradeChange((value && parseInt(value.toString(), 10)) || null)}>
+            <Picker.Item label={'Nicht angeben'} value={null} color={this.getStyles().pickerItems.color}/>
             {
               this.classes.map((i) => {
-                return <Picker.Item label={'' + i} value={i} color={this.getStyles().pickerItems.color}/>
+                return <Picker.Item key={i} label={'' + i} value={i} color={this.getStyles().pickerItems.color}/>
               })
             }
           </Picker>
           {
-            this.selectedYear <= 10 && this.selectedYear != -1 ?
+            this.settings.classSettings.grade <= 10 && this.settings.classSettings.grade != null ?
                 <View>
                   <Text style={this.getStyles().fieldText}>Klasse</Text>
-                  <Picker selectedValue={this.selectedClass}
-                          mode={'dialog'}
+                  <Picker mode={'dialog'}
                           prompt={'Klasse'}
                           itemStyle={this.getStyles().pickerItems}
                           style={this.getStyles().picker}
-                          onValueChange={(v) => {
-                            this.selectedClass = v.toString();
-                          }}>
+                          selectedValue={this.settings.classSettings.className}
+                          onValueChange={(value) => this.onClassChange(value.toString())}>
                     {
                       this.classLetters.map((i) => {
-                        return <Picker.Item label={i} value={i} color={this.getStyles().pickerItems.color}/>
+                        return <Picker.Item key={i} label={i} value={i} color={this.getStyles().pickerItems.color}/>
                       })
                     }
                   </Picker>
                 </View> : null
           }
           {
-            this.selectedYear >= 11 && this.selectedYear != -1 && (
+            this.settings.classSettings.grade >= 11 && (
                 <View>
-                  <CoursePicker onChange={(courses) => {
-                    this.selectedCourses = courses;
-                  }} initialCourses={this.selectedCourses}/>
+                  <CoursePicker onChange={this.onCourseChange.bind(this)}
+                                initialCourses={[...this.settings.classSettings.courses]}/>
                 </View>
             )
           }
           <View>
             <Text style={this.getStyles().fieldText}>Aussehen</Text>
-            <Picker selectedValue={this.theme}
-                    mode={'dialog'}
+            <Picker mode={'dialog'}
                     prompt={'Aussehen'}
                     style={this.getStyles().picker}
                     itemStyle={this.getStyles().pickerItems}
-                    onValueChange={(v) => {
-                      this.theme = v.toString();
-                    }}>
+                    selectedValue={this.settings.theme}
+                    onValueChange={(value) => this.onThemeChange(value.toString())}>
               <Picker.Item label={'Systemeinstellung'} value={'system'} color={this.getStyles().pickerItems.color}/>
               <Picker.Item label={'Hell'} value={'light'} color={this.getStyles().pickerItems.color}/>
               <Picker.Item label={'Dunkel'} value={'dark'} color={this.getStyles().pickerItems.color}/>
