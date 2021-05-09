cd203deb2f91d21bc2a400372c07952339f6ff62
Oliver Enes <oliver@enesnet.de>
2021-05-09 22:02:19 +0200

    fix login

--- src/components/Plan/index.tsx
+++ src/components/Plan/index.tsx
@@ -1,6 +1,6 @@
 import {Component} from "react";
 import * as React from "react";
-import {View, AsyncStorage, StyleSheet, StatusBar} from "react-native";
+import {View, StyleSheet, StatusBar} from "react-native";
 import {observer} from "mobx-react";
 import {observable} from "mobx";
 import {MoodleProvider} from "../../providers/moodle";
@@ -9,42 +9,60 @@ import {PlanView} from "../PlanView";
 import {NavigationScreenProp} from "react-navigation";
 import {Loader} from "../Loader";
 import {ThemeContext} from "../themeContext/theme-context";
+import {ParamsFromLogin} from "../Login";
+import {ClassSettings, SettingsManager} from "../../providers/settings";
 
-interface IPlanProps {
-  navigation: NavigationScreenProp<this>;
+interface PlanProps {
+  navigation: NavigationScreenProp<this, ParamsFromLogin>;
 }
 
 @observer
-export class Plan extends Component<IPlanProps> {
+export class Plan extends Component<PlanProps> {
 
   static contextType = ThemeContext;
 
   @observable
-  private moodleSession: string = '';
+  private readonly moodleSession: string = '';
+
   @observable
   private today: any = {};
+
   @observable
   private tomorrow: any = {};
+
   @observable
-  private studentClass: any = {};
+  private classSettings: ClassSettings = null;
+
   @observable
   private loading: boolean = true;
 
   @observable
   private index: number = 0;
 
+  private settingsManager: SettingsManager;
+
   constructor(props) {
     super(props);
+
+    this.settingsManager = new SettingsManager();
+    this.moodleSession = this.props.navigation.getParam('moodleSession');
+
+    // when coming back from settings
+    // TODO conditional reload
     this.props.navigation.addListener('didFocus', async () => {
       this.loading = true;
       await this.loadPlans();
       await this.readStudentClass();
       this.loading = false;
     });
-    this.loading = true;
-    this.loadPlans();
-    this.readStudentClass();
-    this.loading = false;
+
+    // initially get data from moodle and get class settings
+    Promise.all([
+      this.loadPlans(),
+      this.readStudentClass(),
+    ]).then(() => {
+      this.loading = false;
+    }).catch((error) => console.error(error));
   }
 
   public componentDidMount(): void {
@@ -52,13 +70,13 @@ export class Plan extends Component<IPlanProps> {
   }
 
   public async loadPlans() {
-    this.moodleSession = await AsyncStorage.getItem('moodleSession');
     this.today = await MoodleProvider.getPlan('today', this.moodleSession);
     this.tomorrow = await MoodleProvider.getPlan('tomorrow', this.moodleSession);
   }
 
   public async readStudentClass() {
-    this.studentClass = await AsyncStorage.getItem('class');
+    const settings = await this.settingsManager.load(); // always reload
+    this.classSettings = settings.classSettings;
   }
 
   public render() {
@@ -67,25 +85,29 @@ export class Plan extends Component<IPlanProps> {
             <View style={styles.container}>
               <TabView
                   swipeEnabled={true}
+                  navigationState={{
+                    index: this.index, routes: [
+                      {key: 'first', title: 'Heute'},
+                      {key: 'second', title: 'Morgen'}
+                    ]
+                  }}
+                  onIndexChange={(i) => this.index = i}
                   renderTabBar={(props) => (
                       <TabBar
                           {...props}
+                          style={{backgroundColor: this.context.theme == 'light' ? '#b41019' : '#322f3d'}}
                           indicatorStyle={{backgroundColor: '#FF3333'}}
                           activeColor={this.context.theme == 'light' ? 'white' : '#b41019'}
-                          inactiveColor={'lightgray'}
-                          style={{backgroundColor: this.context.theme == 'light' ? '#b41019' : '#322f3d'}}/>
+                          inactiveColor={'lightgray'}/>
                   )}
-                  navigationState={{
-                    index: this.index, routes: [{key: 'first', title: 'Heute'},
-                      {key: 'second', title: 'Morgen'}]
-                  }}
                   renderScene={SceneMap({
-                    first: () => <PlanView plan={this.today} onRefresh={this.loadPlans.bind(this)}
-                                           studentClass={this.studentClass}/>,
-                    second: () => <PlanView plan={this.tomorrow} onRefresh={this.loadPlans.bind(this)}
-                                            studentClass={this.studentClass}/>
+                    first: () => <PlanView plan={this.today}
+                                           classSettings={this.classSettings}
+                                           onRefresh={this.loadPlans.bind(this)}/>,
+                    second: () => <PlanView plan={this.tomorrow}
+                                            classSettings={this.classSettings}
+                                            onRefresh={this.loadPlans.bind(this)}/>
                   })}
-                  onIndexChange={(i) => this.index = i}
               />
             </View>
         ) : <Loader visible={true} background={false}/>
