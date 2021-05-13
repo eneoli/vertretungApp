0d9bf6334d22371db634f9b0e188743724341143
Oliver Enes <oliver@enesnet.de>
2021-05-13 13:36:03 +0200

    refactoring

--- src/providers/crypto.ts
+++ src/providers/crypto.ts
@@ -4,9 +4,16 @@ import {MoodleProvider} from "./moodle";
 export async function encryptLogin(username: string, password: string): Promise<string> {
   let crypto = new JSEncrypt.JSEncrypt();
   let publicKey = await MoodleProvider.getPublicKey();
-  crypto.setPublicKey(publicKey);
-  return await crypto.encrypt(JSON.stringify({
+
+  const data = JSON.stringify({
     username: username,
-    password: password
-  }));
+    password: password,
+  });
+
+  crypto.setPublicKey(publicKey);
+  const cryptedData = crypto.encrypt(data);
+  if (!cryptedData) {
+    throw new Error('Konnte Daten nicht verschlüsseln');
+  }
+  return cryptedData;
 }
